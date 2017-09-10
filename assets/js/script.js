2c57d2e96eb394716a7f2cdf95781ec9690af5b3
Jonathan Costa <jonjcosta@gmail.com>
2017-09-10 01:00:06 -0300

    Fresh start
    - Redesigned the whole page's basics
    - Added function to choose a side (X or O)
    - Initiated function for checking game state

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,23 @@
 $(function () {
 
-    var filledBlocks = [];
+    /**
+     * First screen lets choose between X or O
+     * When choosing, sets playerIcon and botIcon
+     * Hides the choose a side div
+     * Shows the table
+     * Player clicks a square
+     * Check if it's an empty square
+     * Square is marked with player's symbol
+     * Check the game's state
+     * Create two arrays, each with the ID of the marked square
+     * X: [1, 3, 6...] O: [2, 4, 5...]
+     * If either player's array contains all elements in one of the winnerRows, they win
+     */
 
-    var winnerRows = [
+    var playerIcon = '';
+    var botIcon = '';
+
+    const winnerRows = [
         [1, 2, 3],
         [1, 5, 9],
         [1, 4, 7],
@@ -13,15 +28,78 @@ $(function () {
         [7, 8, 9]
     ];
 
-    $(".block").click(function () {
-        let id = $(this).data("block");
-        if (filledBlocks.includes(id)) return false;
-        filledBlocks.push(id);
-        $(this).html("X");
+    $('.btnSideChoice').click(function (e) { 
+        e.preventDefault();
+        var choice = $(this).data('choice');
+        chooseASide(choice);
     });
-
-    function checkWin() {
-        // if (filledBlocks)
+    
+    $("td").click(function () {
+        if ($(this).html().length > 0) return false;
+        $(this).html(playerIcon);
+        checkGameState();
+        // botMakeAMove();
+    });
+    
+    function chooseASide(playersChoice) {
+        playerIcon = playersChoice == 'X' ? 'X' : 'O';
+        botIcon = playersChoice == 'X' ? 'O' : 'X';
+        $('.chooseASide').hide();
+        $('table').show();
     }
 
+    function botMakeAMove() {
+
+    }
+
+    function checkGameState() {
+        var playerBoard = [];
+        var botBoard = [];
+        $('td').each(function (index, element) {
+            if ($(this).html() == playerIcon) {
+                playerBoard.push(index);
+            } else if ($(this).html() == botIcon) {
+                botBoard.push(index);
+            }
+        });
+
+        $.each(winnerRows, function (index, element) { 
+            if (containsAllElements(element, playerBoard)) {
+                endGame(1);
+                return false;
+            } else if (containsAllElements(element, botBoard)) {
+                endGame(2);
+                return false;
+            }
+        });
+
+        if (isBoardFull) endGame(0);
+
+    }
+
+    function isBoardFull() {
+        $('td').each(function (index, element) {
+            if ( $(this).html().length == 0) return false;
+        });
+        return true;
+    }
+
+    function endGame(status) {
+        switch (status) {
+            case 1:
+                $('.endGameMessage').html("You win!");
+                break;
+            case 2:
+                $('.endGameMessage').html("You lose...");
+                break;
+            default:
+                $('.endGameMessage').html("Draw!");
+        };
+        $('.endGameMessage').fadeIn();
+    }
+
+    function containsAllElements(arrayNeedle, arrayStack) {
+        return false;
+    }
+    
 });
