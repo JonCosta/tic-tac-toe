4cab40a3528812372091a872412dcad965f319c3
Jonathan Costa <jonjcosta@gmail.com>
2017-09-10 21:59:59 -0300

    Bot prototype
    - Added functions to end a game
    - Added multiple properties for game state
    - Added functions to start and reset the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,18 +14,22 @@ $(function () {
      * If either player's array contains all elements in one of the winnerRows, they win
      */
 
-    var playerIcon = '';
+    var againstBot = true;
+    var gameOver = false;    
     var botIcon = '';
+    var playerIcon = '';
+    var currentIcon = '';
+    var playerTurn = 1;
 
     const winnerRows = [
-        [1, 2, 3],
-        [1, 5, 9],
+        [0, 1, 2],
+        [0, 4, 8],
+        [0, 3, 6],
         [1, 4, 7],
         [2, 5, 8],
-        [3, 6, 9],
-        [3, 5, 7],
-        [4, 5, 6],
-        [7, 8, 9]
+        [2, 4, 6],
+        [3, 4, 5],
+        [6, 7, 8]
     ];
 
     $('.btnSideChoice').click(function (e) { 
@@ -35,26 +39,49 @@ $(function () {
     });
     
     $("td").click(function () {
-        if ($(this).html().length > 0) return false;
+        if ($(this).html().length > 0 || gameOver) return false;
         $(this).html(playerIcon);
         checkGameState();
-        // botMakeAMove();
+        if (gameOver) return false;
+        playerTurn = playerTurn == 1 ? 2 : 1;
+        currentIcon = currentIcon == 'X' ? 'O' : 'X';
+        if (againstBot) {
+            console.log('make a move');
+            botMakeAMove();
+        }
+    });
+
+    $('.btnRestart').click(function (e) { 
+        e.preventDefault();
+        restartGame();        
     });
     
     function chooseASide(playersChoice) {
         playerIcon = playersChoice == 'X' ? 'X' : 'O';
         botIcon = playersChoice == 'X' ? 'O' : 'X';
+        currentIcon = playersChoice;
         $('.chooseASide').hide();
         $('table').show();
     }
 
     function botMakeAMove() {
+        var freeSquares = [];
+        $('td').each(function (index, element) {
+            if ($(this).html().length == 0) {
+                freeSquares.push(index);
+            }
+        });
+        console.log(freeSquares);
+        var square = freeSquares[Math.floor(Math.random()*freeSquares.length)];
+        console.log( 'td:nth-child('+square+')');
+        $('td.square'+(square)).html(botIcon);
 
     }
 
     function checkGameState() {
         var playerBoard = [];
         var botBoard = [];
+
         $('td').each(function (index, element) {
             if ($(this).html() == playerIcon) {
                 playerBoard.push(index);
@@ -72,34 +99,54 @@ $(function () {
                 return false;
             }
         });
-
-        if (isBoardFull) endGame(0);
-
+        if (isBoardFull()) endGame(0);
     }
 
     function isBoardFull() {
+        var isFull = true;
         $('td').each(function (index, element) {
-            if ( $(this).html().length == 0) return false;
+            if ( $(this).html().length == 0 ) {
+                isFull = false;
+            } 
         });
-        return true;
+        return isFull;
     }
 
     function endGame(status) {
         switch (status) {
+            case 0:
+                $('.endGameMessage').html("Draw!");
+                break;
             case 1:
                 $('.endGameMessage').html("You win!");
                 break;
             case 2:
                 $('.endGameMessage').html("You lose...");
                 break;
-            default:
-                $('.endGameMessage').html("Draw!");
         };
-        $('.endGameMessage').fadeIn();
+        gameOver = true;
+        $('.endGame').fadeIn();
     }
 
     function containsAllElements(arrayNeedle, arrayStack) {
-        return false;
+        for (var i = 0; i < arrayNeedle.length; i++) {
+            var element = arrayNeedle[i];
+            if (arrayStack.indexOf(element) < 0) return false;
+        }
+        return true;
+    }
+
+    function restartGame() {
+        $('td').html('');
+        $('table').fadeOut();
+        $('.chooseASide').fadeIn();
+        $('.endGame').fadeOut();
+        $('.endGameMessage').html('');
+        playerIcon = '';
+        botIcon = '';
+        playerTurn = 1;
+        currentIcon = '';
+        gameOver = false;
     }
     
 });
