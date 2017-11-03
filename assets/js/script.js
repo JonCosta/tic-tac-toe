dcf9ed65d8e3b9df38af32dd9a779ee70df83926
Jonathan Costa <jonjcosta@gmail.com>
2017-11-02 22:27:09 -0200

    Beginning game logic
    - Added functions to choose side and reveal the game table
    - Added function to mark blocks in the game table with the chosen icon
    - Beginning logic for checking end game event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,48 @@
+var Game = {
+    
+    iconO: '<i class="fa fa-circle-o"></i>',
+    iconX: '<i class="fa fa-times"></i>',
+    playerChoice: '',
+    botChoice: '',
+    playerMarkedCells: [],
+    botMarkedCells: [],
+    winningRows: [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+        [3, 5, 7],
+        [1, 5, 9]
+    ],
+
+    resetGame: function() {
+        this.botChoice = this.playerChoice = '';
+        this.playerMarkedCells = this.botMarkedCells = [];
+    },
+
+    checkIfGameOver: function(choiceArray) {
+        if (choiceArray.length < 3) return false;
+        for (let i = 0; i < Game.winningRows.length; i++) {
+            let row = Game.winningRows[i];
+            for (let j = 0; j < row.length; j++) {
+                if (choiceArray.indexOf(row[j]) < 0) {
+                    // Victory!
+                }
+            }
+        }
+        // If no winners, check for draw
+        if (Game.playerMarkedCells.length + Game.botMarkedCells.length == 9) {
+                
+        }
+    }
+
+};
+
 $(function() {
+
+    Game.resetGame();
 
     $('.b-choice').fadeIn();
 
@@ -6,7 +50,17 @@ $(function() {
         e.preventDefault();
         let side = $(this).data("choice");
         console.log(`You chose ${side}!`);
+        Game.playerChoice = side == 'X' ? Game.iconX : Game.iconO;
+        Game.botChoice = side == 'X' ? Game.iconO : Game.iconX;
         $('.b-choice').fadeOut();
+        $('.b-game').fadeIn();
+    });
+
+    $('.game--table td').click(function(e) {
+        let cell = $(this).data('cell');
+        Game.playerMarkedCells.push(cell);
+        console.log(Game.playerMarkedCells);
+        $(this).html(Game.playerChoice);
     });
 
 });
